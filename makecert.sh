1edea12f2336dd54c371a301dbe8d16d854ade95
hdznrrd <hadez@nrrd.de>
2012-05-04 16:49:51 +0200

    created cert folder and modified helper script to create certs inside this folder

--- makecert.sh
+++ makecert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout karma.shack.key -out karma.shack.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout cert/karma.key -out cert/karma.crt
